c335b12f2440e2766d32b96a78ce473f94bf6007
7ldsma <alvaro.ledesmav@gmail.com>
2023-06-14 12:49:01 +0200

    First commit. fetch function.

--- salida.js
+++ salida.js
@@ -1,1 +1,29 @@
-var API_URL = 'https://icanhazdadjoke.com/';
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function getJokes() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch('https://icanhazdadjoke.com/', {
+                headers: {
+                    'Accept': 'application/json',
+                }
+            });
+            const data = yield response.json();
+            const joke = data.joke;
+            console.log(data);
+            console.log(joke);
+        }
+        catch (error) {
+            console.error('Error al obtener la broma', error);
+        }
+    });
+}
+getJokes();
--- salida.ts
+++ salida.ts
@@ -1,2 +1,22 @@
-const API_URL = 'https://icanhazdadjoke.com/';
 
+async function getJokes(){
+
+    try {
+
+        const response = await fetch('https://icanhazdadjoke.com/', {
+            headers: {
+                'Accept': 'application/json',
+            }
+        });
+
+        const data = await response.json();
+        const joke = data.joke;
+        console.log(data);
+        console.log(joke);  
+    } catch (error) {
+        console.error('Error al obtener la broma', error);
+    }
+
+}
+
+getJokes();
